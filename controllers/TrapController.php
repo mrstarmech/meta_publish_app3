1c03a40a6d9b752f804d3a3956346fed2e7bb5e7
mrstarmech <mr.starmech@gmail.com>
2022-02-28 04:38:30 +0700

    check if param set

--- controllers/TrapController.php
+++ controllers/TrapController.php
@@ -9,47 +9,55 @@ use Yii;
 
 class TrapController extends \yii\rest\Controller
 {
+    function getParam($name, $from)
+    {
+        if(isset($from[$name])) {
+            return $from[$name];
+        }
+        return '';
+    }
+
     public function actionLog()
     {
         if(Yii::$app->request->isPost) {
             $data = json_decode(file_get_contents('php://input'),true);
             if($data === null) return 'data null';
             $model = new Click();
-            $model->requestId = $data['requestId'];
-            $model->tag = json_encode($data['tag']);
-            $model->linkedId = $data['linkedId'];
-            $model->visitorId = $data['visitorId'];
-            $model->visitorFound = intval(boolval($data['visitorFound']));
-            $model->timestamp = intval($data['timestamp']);
-            $model->time = gmdate('d.m.Y - G:i:s', intval($data['timestamp'])/1000);
-            $model->incognito = intval(boolval($data['incognito']));
-            $model->url = $data['url'];
-            $model->clientReferer = $data['clientReferrer'];
-            $model->ip = $data['ip'];
+            $model->requestId = $this->getParam('requestId',$data);
+            $model->tag = json_encode($this->getParam('tag',$data));
+            $model->linkedId = $this->getParam('linkedId',$data);
+            $model->visitorId = $this->getParam('visitorId',$data);
+            $model->visitorFound = intval(boolval($this->getParam('visitorFound',$data)));
+            $model->timestamp = intval($this->getParam('timestamp',$data));
+            $model->time = gmdate('d.m.Y - G:i:s', intval($this->getParam('timestamp',$data))/1000);
+            $model->incognito = intval(boolval($this->getParam('incognito',$data)));
+            $model->url = $this->getParam('url',$data);
+            $model->clientReferer = $this->getParam('clientReferrer',$data);
+            $model->ip = $this->getParam('ip',$data);
             $ipLocation = new IpLocation();
-            $dataIpLocation = $data['ipLocation'];
-            $ipLocation->city = json_encode($dataIpLocation['city']);
-            $ipLocation->continent = json_encode($dataIpLocation['continent']);
-            $ipLocation->country = json_encode($dataIpLocation['country']);
-            $ipLocation->latitude = floatval($dataIpLocation['latitude']);
-            $ipLocation->longitude = floatval($dataIpLocation['longitude']);
-            $ipLocation->postalCode = $dataIpLocation['postalCode'];
-            $ipLocation->subdivisions = json_encode($dataIpLocation['subdivisions']);
-            $ipLocation->timezone = $dataIpLocation['timezone'];
+            $dataIpLocation = $this->getParam('ipLocation',$data);
+            $ipLocation->city = json_encode($this->getParam('city',$dataIpLocation));
+            $ipLocation->continent = json_encode($this->getParam('continent',$dataIpLocation));
+            $ipLocation->country = json_encode($this->getParam('country',$dataIpLocation));
+            $ipLocation->latitude = floatval($this->getParam('latitude',$dataIpLocation));
+            $ipLocation->longitude = floatval($this->getParam('longitude',$dataIpLocation));
+            $ipLocation->postalCode = $this->getParam('postalCode',$dataIpLocation);
+            $ipLocation->subdivisions = json_encode($this->getParam('subdivisions',$dataIpLocation));
+            $ipLocation->timezone = $this->getParam('timezone',$dataIpLocation);
             if($ipLocation->save()) {
                 $model->ipLocationId = $ipLocation->id;
             } else {
                 $model->ipLocationId = -1;
             }
             $browserDetails = new Browserdetail();
-            $dataBrowserDetails = $data['browserDetails'];
-            $browserDetails->name = $dataBrowserDetails['browserName'];
-            $browserDetails->fullVersion = $dataBrowserDetails['browserFullVersion'];
-            $browserDetails->majorVersion = $dataBrowserDetails['browserMajorVersion'];
-            $browserDetails->os = $dataBrowserDetails['os'];
-            $browserDetails->osVersion = $dataBrowserDetails['osVersion'];
-            $browserDetails->device = $dataBrowserDetails['device'];
-            $browserDetails->userAgent = $dataBrowserDetails['userAgent'];
+            $dataBrowserDetails = $this->getParam('browserDetails',$data);
+            $browserDetails->name = $this->getParam('browserName',$dataBrowserDetails);
+            $browserDetails->fullVersion = $this->getParam('browserFullVersion',$dataBrowserDetails);
+            $browserDetails->majorVersion = $this->getParam('browserMajorVersion',$dataBrowserDetails);
+            $browserDetails->os = $this->getParam('os',$dataBrowserDetails);
+            $browserDetails->osVersion = $this->getParam('osVersion',$dataBrowserDetails);
+            $browserDetails->device = $this->getParam('device',$dataBrowserDetails);
+            $browserDetails->userAgent = $this->getParam('userAgent',$dataBrowserDetails);
             if($browserDetails->save()) {
                 $model->browserDetailsId = $browserDetails->id;
             } else {
