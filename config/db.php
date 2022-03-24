9aad7af1d91521cdfee2070853cc4fe0d03c4f10
autocommit_etckeeper <hostinfo@metacpa.ru>
2022-03-24 02:44:27 +0000

    back

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=meta_app',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'starmech',
+    'password' => 'hjvfy8008dkfc.r',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
